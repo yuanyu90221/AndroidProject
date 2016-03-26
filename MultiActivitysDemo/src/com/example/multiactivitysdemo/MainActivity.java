f9b2fd7f734819088f28c61f2192dc4f72965bb0
yuanyu90221 <yuanyu90221@gmail.com>
2016-03-27 00:39:55 +0800

    change Comments

--- MultiActivitysDemo/src/com/example/multiactivitysdemo/MainActivity.java
+++ MultiActivitysDemo/src/com/example/multiactivitysdemo/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.multiactivitysdemo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends Activity {
