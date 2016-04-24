f862e8a99f7118e0e0c7439db037185e42a6ce73
yuanyu90221 <yuanyu90221@gmail.com>
2016-04-24 23:27:20 +0800

    移除部必要的變數

--- DraggablePuzzleDemo/src/com/example/draggablepuzzledemo/FourthActivity.java
+++ DraggablePuzzleDemo/src/com/example/draggablepuzzledemo/FourthActivity.java
@@ -7,7 +7,6 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
