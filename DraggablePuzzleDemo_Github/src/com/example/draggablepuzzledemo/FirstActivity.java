b7d4561f1f37a90a4c8f67216d2438ad08615dff
yuanyu90221 <yuanyu90221@gmail.com>
2016-05-01 12:26:33 +0800

    移除部必要的import

--- DraggablePuzzleDemo_Github/src/com/example/draggablepuzzledemo/FirstActivity.java
+++ DraggablePuzzleDemo_Github/src/com/example/draggablepuzzledemo/FirstActivity.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import com.example.draggablepuzzledemo.EasyLevelActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
